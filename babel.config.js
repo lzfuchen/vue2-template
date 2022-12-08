85190d91e836b40d0d8910beab2e47131adbe869
fuchen <284992984@qq.com>
2022-12-08 14:03:15 +0800

    feat: 添加babel

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,13 @@
-module.exports = {}
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+        modules: false
+      }
+    ]
+  ],
+  plugins: ['@babel/plugin-transform-runtime']
+}
