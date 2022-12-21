9dce676e306d6dbfcf2ee19af227a9d8839fffb9
fuchen <284992984@qq.com>
2022-12-21 17:37:35 +0800

    config

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,13 +4,13 @@ module.exports = {
   // 换行的行长
   printWidth: 120,
   // 没有尾随逗号
-  trailingComma: "none",
+  trailingComma: 'none',
   // 尽可能省略箭头函数括号
-  "arrowParens": "avoid",
+  arrowParens: 'avoid',
   // 每个语句的末尾去除分号
-  "semi": false,
+  semi: false,
   // 使用单引号而不是双引号
-  "singleQuote": true,
-  "endOfLine": "auto",
-  "proseWrap": "never"
+  singleQuote: true,
+  endOfLine: 'auto',
+  proseWrap: 'never'
 }
